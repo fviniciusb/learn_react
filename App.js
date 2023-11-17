2dc187df37775f2e8b95c5f7131126b596a232e6
Francisco Vinicius <106833771+fviniciusb@users.noreply.github.com>
2023-11-17 17:03:08 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(90deg, #383838 0%, #000000 81.25%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+`;
+
+export const TodoList = styled.div`
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+
+`;
+
+export const List = styled.ul`
+    padding: 0; 
+    
+`;
+
+
+export const Item = styled.li`
+    color: #000;
+    font-size: 15px;
+    font-weight: 400;
+    background: #e4e4e4;
+    box-shadow: 1px 4px 10px rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+    height: 60px;
+    list-style-type:none;
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Input = styled.input`
+    border: 2px solid rgba(209, 211, 212, 0.4);
+    border-radius: 5px;
+    height: 40px;
+    font-size: 15px;
+    outline: none;
+    padding-left: 10px;
+
+`;
+
+export const Button = styled.button`
+    border-radius: 5px;
+    background: #8052ec;
+    height: 40px;
+    border: none;
+    color: #FFFFFF;
+    cursor: pointer;
+    margin-left: 20px;
+
+`;
